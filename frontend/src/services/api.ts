8291f0d2145b4c734ac0ba84c88209529fae139f
Roberto Lara <55064373+betolara1@users.noreply.github.com>
2026-08-15 17:36:48 -0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Instancia do axios para fazer chamada pro Backend
 export const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
